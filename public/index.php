dcb45d6d51a85ea6d950a0994bc9ea1137c42629
olwcn <40833379+olwcn@users.noreply.github.com>
2024-03-26 07:50:27 +0000

    dışarıdan ilk MR/PR artık ne demek istersen

--- public/index.php
+++ public/index.php
@@ -1,3 +1,20 @@
 <?php
 
 declare(strict_types = 1);
+
+echo "selam";
+
+$servername = "db";
+$username = "user";
+$password = "user";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=framework", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
